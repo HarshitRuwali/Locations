6826dd696be271e3042e21d4c52f283cd692c5fc
HarshitRuwali <harshitruwali@outlook.com>
2020-01-08 04:29:37 +0530

    Initial Commit

---
+++ Locations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Locations
+//
+//  Created by Harshit Ruwali on 08/01/20.
+//  Copyright © 2020 Harshit Ruwali. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
